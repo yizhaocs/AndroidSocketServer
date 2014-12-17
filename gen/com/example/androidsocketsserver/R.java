d592380a97e841479df665f0b61a84fc7d17b02b
Yi Zhao <yizhao.cs@gmail.com>
2014-12-16 18:59:06 -0800

    1

--- gen/com/example/androidsocketsserver/R.java
+++ gen/com/example/androidsocketsserver/R.java
@@ -18,7 +18,8 @@ public final class R {
         public static final int textView1=0x7f060000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_android_sockets_client=0x7f030000;
+        public static final int activity_android_sockets_server=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
