3663394b41870af84430faf4db817b48e29cf423
Yi Zhao <yizhao.cs@gmail.com>
2014-12-30 17:36:24 -0800

    12-30-2014-24

---
+++ src/com/example/androidsocketsserver/AndroidSocketsRecieverForDragAndDropSetViewV7.java
@@ -0,0 +1,409 @@
+package com.example.androidsocketsserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ClipData;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.DragEvent;
+import android.view.MotionEvent;
+import android.view.MotionEvent.PointerCoords;
+import android.view.View;
+import android.view.View.DragShadowBuilder;
+import android.view.View.OnDragListener;
+import android.view.View.OnLongClickListener;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class AndroidSocketsRecieverForDragAndDropSetViewV7 extends Activity {
+	private Boolean exit = false;
+	private Boolean isDragging = false;
+
+	private List<View> viewsList;
+	// private View movingView = null;
+	private AndroidSocketsRecieverForDragAndDropSetViewV7 a = this;
+	ConvertorOfJsonObjectToMotionEvent mConvertorOfJsonObjectToMotionEvent = ConvertorOfJsonObjectToMotionEvent.getInstance();
+	Bitmap mBitmap;
+
+	/** Called when the activity is first created. */
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.recieverfordraganddrop);
+		viewsList = ViewTraversal.travasalViews(findViewById(R.id.rootview));
+		for (View v : viewsList) {
+			Log.d("viewsList", String.valueOf(v.getId()));
+		}
+		findViewById(R.id.myimage1).setOnTouchListener(mOnTouchListener);
+		findViewById(R.id.myimage1).setOnLongClickListener(mOnLongClickListener);
+
+		// findViewById(R.id.topleft).setOnDragListener(new BackgroundViewsDragListener());
+		// findViewById(R.id.topright).setOnDragListener(new BackgroundViewsDragListener());
+
+		MyTask mSocketsServer = new MyTask();
+		mSocketsServer.execute();
+
+	}
+
+	private OnTouchListener mOnTouchListener = new OnTouchListener() {
+		@SuppressLint("ClickableViewAccessibility")
+		public boolean onTouch(View view, MotionEvent motionEvent) {
+			ViewGroup rootView = (ViewGroup) findViewById(R.id.rootview);
+			View topleft = findViewById(R.id.topleft);
+			View topright = findViewById(R.id.topright);
+			LinearLayout topleftGroupView = (LinearLayout) topleft;
+			LinearLayout toprightGroupView = (LinearLayout) topright;
+			
+			switch (motionEvent.getAction()) {
+			case MotionEvent.ACTION_DOWN:
+				Log.d("motionEvent", "ACTION_DOWN");
+				isDragging = true;
+				mBitmap = getBitmapFromView(view);
+				ViewGroup firstOuterLayout = (ViewGroup) view.getParent();
+				if (firstOuterLayout.getId() == R.id.topleft || firstOuterLayout.getId() == R.id.topright) {
+					firstOuterLayout.removeView(view);
+					//firstOuterLayout.removeViewInLayout(view);
+					rootView.addView(view);
+				}
+
+				// rootView.bringChildToFront(view);
+				return true;
+			case MotionEvent.ACTION_MOVE:
+				Log.d("motionEvent", "ACTION_MOVE");
+				if (isDragging) {
+					view.setX(motionEvent.getRawX() - view.getWidth() / 2);
+					view.setY(motionEvent.getRawY() - view.getHeight() - view.getHeight() / 2);
+					//view.requestLayout();
+					return true;
+				} else {
+					return false;
+				}
+			case MotionEvent.ACTION_UP:
+				Log.d("motionEvent", "ACTION_UP");
+				isDragging = false;
+
+				// int location[] = new int[2];
+				// view.getLocationOnScreen(location);
+				// int viewX = location[0];
+				// int viewY = location[1];
+				// View newView = ViewTraversal.getView(viewX, viewY, viewsList);
+				
+//				ViewGroup owner3 = (ViewGroup) view.getParent();
+//				if (owner3 != null && (owner3.getId() == R.id.topleft || owner3.getId() == R.id.topright)) {
+//					owner3.removeView(view);
+//				}
+				float curX = motionEvent.getX();
+				float curY = motionEvent.getY();
+//				Log.d("Coord", "X:" + String.valueOf(curX) + ", " + "Y:" + String.valueOf(curY));
+//				View underView = ViewTraversal.getView(curX, curY, viewsList);
+				if(ViewTraversal.isPointInsideView(curX, curY, topleft)){
+					rootView.removeView(view);
+					//rootView.removeViewInLayout(view);
+					view.setX(topleft.getX());
+					view.setY(topleft.getY());
+					topleftGroupView.addView(view);
+					topleftGroupView.bringChildToFront(view);
+				}else if(ViewTraversal.isPointInsideView(curX, curY, topright)){
+					rootView.removeView(view);
+					//rootView.removeViewInLayout(view);
+					view.setX(topright.getX());
+					view.setY(topright.getY());
+					toprightGroupView.addView(view);
+					toprightGroupView.bringChildToFront(view);
+				}else{
+					rootView.removeView(view);
+					//rootView.removeViewInLayout(view);
+					view.setX(topleft.getX());
+					view.setY(topleft.getY());
+					topleftGroupView.addView(view);
+					topleftGroupView.bringChildToFront(view);
+				}
+				view.requestLayout();
+//				LinearLayout underViewToGroupView = (LinearLayout) underView;
+//				if (underViewToGroupView != null) {
+//					view.setX(underViewToGroupView.getX());
+//					view.setY(underViewToGroupView.getY());
+//			
+//					underViewToGroupView.addView(view);
+//					underViewToGroupView.bringChildToFront(view);
+//				} else {
+//					rootView.removeView(view);
+//					View ax = findViewById(R.id.topright);
+//					LinearLayout sd =  (LinearLayout) ax;
+//					sd.addView(view);
+//					view.setX(sd.getX());
+//					view.setY(sd.getY());
+//					sd.bringChildToFront(view);
+//				}
+				return true;
+			default:
+				break;
+			}
+			return false;
+		}
+	};
+
+	private void trigerDragAndDrop(View view) {
+		SimpleDragShadow mSimpleDragShadow = new SimpleDragShadow(view);
+		// DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+		ClipData data = ClipData.newPlainText("", "");
+		view.startDrag(data, mSimpleDragShadow, view, 0);
+		view.setVisibility(View.INVISIBLE);
+	}
+
+	private void showToast(Toast toast, int position, String info) {
+		// toast = Toast.makeText(getApplicationContext(), info, Toast.LENGTH_SHORT);
+		// toast.setGravity(position, 0, 0);
+		// toast.show();
+	}
+
+	private void cancelToast(Toast toast_1, Toast toast_2, Toast toast_3) {
+
+		// if (toast_1 != null) {
+		// toast_1.cancel();
+		// }
+		// if (toast_2 != null) {
+		// toast_2.cancel();
+		// }
+		// if (toast_3 != null) {
+		// toast_3.cancel();
+		// }
+	}
+
+	private OnLongClickListener mOnLongClickListener = new OnLongClickListener() {
+		@Override
+		public boolean onLongClick(View view) {
+			Log.d("onLongClick", "onLongClick");
+			// trigerDragAndDrop(view);
+			return false;
+		}
+	};
+
+	protected void dispatchView(final View v, final MotionEvent event) {
+		this.runOnUiThread(new Runnable() {
+			public void run() {
+				dispatchTouchEvent(event);
+			}
+		});
+	}
+
+	class BackgroundViewsDragListener implements OnDragListener {
+		@Override
+		public boolean onDrag(View layoutview, DragEvent dragEvent) {
+			switch (dragEvent.getAction()) {
+			case DragEvent.ACTION_DRAG_STARTED:
+				Log.d("dragEvent", "Drag event started");
+				break;
+			case DragEvent.ACTION_DRAG_ENTERED:
+				Log.d("dragEvent", "Drag event entered into " + layoutview.toString());
+				break;
+			case DragEvent.ACTION_DRAG_EXITED:
+				Log.d("dragEvent", "Drag event exited from " + layoutview.toString());
+				break;
+			case DragEvent.ACTION_DROP:
+				Log.d("dragEvent", "Dropped");
+				// Dropped, reassign View to ViewGroup
+				View dragView = (View) dragEvent.getLocalState();
+				ViewGroup owner = (ViewGroup) dragView.getParent();
+				owner.removeView(dragView);
+				LinearLayout container = (LinearLayout) layoutview;
+				container.addView(dragView);
+				dragView.setVisibility(View.VISIBLE);
+				break;
+			case DragEvent.ACTION_DRAG_ENDED:
+				Log.d("dragEvent", "Drag ended");
+				break;
+			default:
+				break;
+			}
+			return true;
+		}
+
+	}
+
+	public class MyTask extends AsyncTask<String, String, String> {
+		@Override
+		protected void onPreExecute() {
+			// super.onPreExecute();
+			// updateDisplay("Starting task" + "\n");
+		};
+
+		@SuppressWarnings("resource")
+		protected String doInBackground(String... params) {
+			try {
+				int portNumber = 4444;
+				boolean listening = true;
+				ServerSocket serverSocket = new ServerSocket(portNumber);
+				while (listening) {
+					new MultiSocketsServerThread(serverSocket.accept(), viewsList).start();
+				}
+			} catch (IOException e) {
+				Log.e("error", "IOException:" + e.getMessage());
+			}
+			return "Task complete";
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			// TODO Auto-generated method stub
+			// super.onPostExecute(result);
+			// updateDisplay(result);
+		}
+
+		@Override
+		protected void onProgressUpdate(String... values) {
+			// TODO Auto-generated method stub
+			// super.onProgressUpdate(values);
+			// updateDisplay(values[0]);
+		}
+	}
+
+	@Override
+	public void onBackPressed() {
+		if (exit) {
+			finish(); // finish activity
+		} else {
+			Toast.makeText(this, "Press Back again to Exit.", Toast.LENGTH_SHORT).show();
+			exit = true;
+			new Handler().postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					exit = false;
+				}
+			}, 3 * 1000);
+
+		}
+
+	}
+
+	public class MultiSocketsServerThread extends Thread {
+
+		private Socket socket = null;
+		private List<View> viewsList;
+
+		public MultiSocketsServerThread(Socket socket, List<View> viewsList) {
+			super("KKMultiServerThread");
+			this.socket = socket;
+			this.viewsList = viewsList;
+
+		}
+
+		@SuppressLint("NewApi")
+		public void run() {
+
+			try {
+				// PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+				String inputLine;
+
+				// out.println("connection is setup between server and client");
+
+				while ((inputLine = in.readLine()) != null) {
+					JSONObject jo = new JSONObject(inputLine);
+					Log.d("whatf", jo.toString());
+					MotionEvent me = mConvertorOfJsonObjectToMotionEvent.createMotionEvent(jo);
+					PointerCoords outPointerCoords = new PointerCoords();
+					me.getPointerCoords(0, outPointerCoords);
+					Log.d("haha", me.toString());
+					Log.d("hahax", String.valueOf(outPointerCoords.x));
+					Log.d("hahay", String.valueOf(outPointerCoords.y));
+					//
+					// View v = ViewTraversal.getView(outPointerCoords.x, outPointerCoords.y, viewsList);
+					// // Log.d("hahaID", String.valueOf(v.getId()));
+					// if (me != null && v != null) {
+					// Log.d("haha", "dispatchTouchEvent");
+					// // v.dispatchTouchEvent(me);
+					// //a.dispatchView(v, me);
+					// //a.dispatchTouchEvent(me);
+					//
+					// }
+					a.dispatchView(null, me);
+					// a.dispatchTouchEvent(me);
+					// out.println("echo " + inputLine);
+					if (inputLine.equals("Bye")) {
+						break;
+					}
+				}
+				socket.close();
+			} catch (IOException e) {
+				Log.e("error", "IOException:" + e.getMessage());
+
+			} catch (JSONException e) {
+				Log.e("error", "JSONException:" + e.getMessage());
+
+			}
+		}
+
+	}
+
+	private Bitmap getBitmapFromView(View view) {
+		// Define a bitmap with the same size as the view
+		Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+		// Bind a canvas to it
+		Canvas canvas = new Canvas(returnedBitmap);
+		// Get the view's background
+		Drawable bgDrawable = view.getBackground();
+		if (bgDrawable != null)
+			// has background drawable, then draw it on the canvas
+			bgDrawable.draw(canvas);
+		else
+			// does not have background drawable, then draw white background on the canvas
+			canvas.drawColor(Color.WHITE);
+		// draw the view on the canvas
+		view.draw(canvas);
+		// return the bitmap
+		return returnedBitmap;
+	}
+
+	private class SimpleDragShadow extends DragShadowBuilder {
+		ColorDrawable greyBox;
+
+		public SimpleDragShadow(View view) {
+			super(view);
+			greyBox = new ColorDrawable(Color.LTGRAY);
+		}
+
+		@Override
+		public void onDrawShadow(Canvas canvas) {
+			// super.onDrawShadow(canvas);
+			greyBox.draw(canvas);
+			;
+		}
+
+		@Override
+		public void onProvideShadowMetrics(Point shadowSize, Point shadowTouchPoint) {
+			View v = getView();
+			int height = (int) v.getHeight() / 2;
+			int width = (int) v.getWidth() / 2;
+
+			greyBox.setBounds(0, 0, width, height);
+			shadowSize.set(width, height);
+			shadowTouchPoint.set((int) width / 2, (int) height / 2);
+
+			// super.onProvideShadowMetrics(shadowSize, shadowTouchPoint);
+		}
+	}
+
+}
